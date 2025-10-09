788eed53aac3c7f5c5ecbfd059688cc8cd8764c6
Roberto Jimenez <robertojj2006@gmail.com>
2025-10-09 20:14:14 +0200

    mas cosas

--- importdataset.py
+++ importdataset.py
@@ -1,17 +1,12 @@
-# Importar librerías necesarias
 import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.utils import to_categorical
 
-# Cargar los datos
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
-# Verificar dimensiones y tipos de datos
-print("x_train shape:", x_train.shape)
-print("y_train shape:", y_train.shape)
-print("x_test shape:", x_test.shape)
-print("y_test shape:", y_test.shape)
-print("Tipo de x_train:", x_train.dtype)
-print("Tipo de y_train:", y_train.dtype)
-
+def load_cifar10():
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    print("x_train shape:", x_train.shape)
+    print("y_train shape:", y_train.shape)
+    print("x_test shape:", x_test.shape)
+    print("y_test shape:", y_test.shape)
+    print("Tipo de x_train:", x_train.dtype)
+    print("Tipo de y_train:", y_train.dtype)
+    return x_train, y_train, x_test, y_test
