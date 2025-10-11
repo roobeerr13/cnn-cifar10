aae09d3bafd23ac3e1e953694d9bc8394ad4b892
MarcoUnie <mmagecoyt@gmail.com>
2025-10-11 15:15:01 +0200

    creación del modelo a partir de las condiciones de la tarea

---
+++ modelo.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def crear_modelo(input_shape=(64, 64, 3), num_classes=10):
+    model = models.Sequential([
+
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+
+    return model
+
+
+if __name__ == "__main__":
+    modelo = crear_modelo()
+    modelo.summary()  # Muestra la arquitectura del modelo
